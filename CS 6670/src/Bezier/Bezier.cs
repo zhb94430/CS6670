a51bd1706fa3e8c5df511da91024b35d786d7726
Hongbin Zhang <zhan2385@umn.edu>
2018-09-20 19:55:32 -0600

    Some Basic Structures

---
+++ CS 6670/src/Bezier/Bezier.cs
@@ -0,0 +1,19 @@
+using System;
+namespace Application
+{
+    //TODO Raster Function
+    //     Evaluate
+
+
+    public class Bezier
+    {
+        // Data Structures
+        float[][] PArray;
+
+        // Methods
+        public Bezier(float[][] _PArray)
+        {
+            PArray = _PArray;
+        }
+    }
+}
