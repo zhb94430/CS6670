9670e67524316e8b1db33bc38dfc09a6a236cdb1
Hongbin Zhang <zhan2385@umn.edu>
2018-09-22 23:30:47 -0600

    Parser Almost Ready

--- src/FileManagement/DatParser.cs
+++ src/FileManagement/DatParser.cs
@@ -9,41 +9,39 @@ namespace src
     public class DatParser
     {
         // Arrays to store the points
-        float[][] PArray;
-
-        Bezier b = new Bezier(PArray);
+        float[,] PArray;
 
         public DatParser(string fileLocation)
         {
-            if (File.Exists(fileLocation))
-            {
-                Parse(new StreamReader(path));
-            }
-            else
-            {
-                Console.WriteLine("Invalid Path for DatParser");
-            }
+            //TODO Try-catch
+
+                this.Parse(new StreamReader(fileLocation));
+
         }
 
         private void Parse(StreamReader r)
         {
-            while(r.Peek() >= 0)
+            int numberOfLines = -1;
+            int arrayLength = 0;
+            int currentIndex = 0;
+
+            while(r.Peek() >= 0 && numberOfLines != 0)
             {
                 string currentLine = r.ReadLine();
 
                 // # Comment
-                if (currentLine.StartsWith("#"))
+                if (currentLine.StartsWith("#", StringComparison.Ordinal))
                 {
                     continue;
                 }
 
                 // Parse the line
-                string[] parsed = currentLine.Split(" ");
+                string[] parsed = currentLine.Split(' ');
 
                 // Number of lines
                 if (parsed.Length == 1)
                 {
-
+                    numberOfLines = int.Parse(parsed[0]);
                 }
 
                 // Number of Points & Points Type
@@ -52,22 +50,46 @@ namespace src
                     // Record amount of points and init array
                     if (parsed[0].Equals("Q") || parsed[0].Equals("P"))
                     {
+                        arrayLength = int.Parse(parsed[1]);
 
+                        PArray = new float[2, arrayLength];
+                        currentIndex = 0;
                     }
+
                     // Record Points
-                    else
+                    else if (currentIndex < arrayLength)
                     {
                         // Q Points
                         if (parsed.Length == 4)
                         {
+                            float x = float.Parse(parsed[1]) / float.Parse(parsed[3]);
+                            float y = float.Parse(parsed[2]) / float.Parse(parsed[3]);
 
+                            PArray[0, currentIndex] = x;
+                            PArray[1, currentIndex] = y;
+                            currentIndex++;
                         }
 
                         // P Points
                         else if (parsed.Length == 3)
                         {
+                            float x = float.Parse(parsed[1]);
+                            float y = float.Parse(parsed[2]);
 
+                            PArray[0, currentIndex] = x;
+                            PArray[1, currentIndex] = y;
+                            currentIndex++;
                         }
+                    }
+
+                    // Generate Bezier Line
+                    else if (currentIndex == arrayLength)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Generating Bezier Line with ");
+                        System.Diagnostics.Debug.WriteLine(arrayLength);
+
+                        currentIndex = 0;
+                        arrayLength = 0;
                     }
                 }
             }
